7de26787268b762bc7226324c88c4fa332969224
miriamCR <miriamcabana96@gmail.com>
2021-05-20 18:29:18 +0200

    mario_samll.json editado y game.js hasta el 28-4 (no incluido) valido)

--- game.js
+++ game.js
@@ -6,7 +6,12 @@ var game = function() {
 var Q = window.Q = Quintus()
 	       .include("Sprites, Scenes, Input, 2D, Anim, Touch, UI, TMX")
 	       // Maximize this game to whatever the size of the browser is
-	       .setup('myGame')
+	       .setup('myGame',
+	       {
+	       	width: 800,
+	       	height: 600,
+	       	scaleToFit : true //Scale the game to fit the screen of the player´s device
+	       })
 	       // And turn on default input controls and touch input (for UI)
 	       .controls()
 	       .touch();
@@ -14,17 +19,41 @@ var Q = window.Q = Quintus()
 	Q.Sprite.extend("Mario",{
 			init: function(p) {
 				this._super(p, {
-					 sheet: "marioR",
+					 sheet: "mario",
+					 sprite: "mario_anim",
 					 x: 250,
 					 y: 250,
 					 frame: 0,
-					 scale: 3
+					 scale: 1
 				 });
-				Q.input.on("left", this, function(){ this.p.x -= 10;});
-				Q.input.on("right", this, function(){ this.p.x += 10;});
-				Q.input.on("up", this, function(){ this.p.y -= 10;});
-				Q.input.on("down", this, function(){ this.p.y += 10;});
+				//el 2d es para darle gravedad y que este sobre el mapa y choque correctamente con la plataforma, no flotando
+				//el platf...para darle controles de juego, arriba, abajo, der, izq
+				this.add("2d, platformerControls, animation"); //componentes
+			},
+			step: function(dt){
+				if(this.p.vx > 0){
+					this.play("walk_right");
+				} else if(this.p.vx < 0){
+					this.play("walk_left");
+				}
+
+				if(this.p.vy < 0){
+					if(this.p.vx > 0){
+						this.play("jump_right");
+					} else if(this.p.vx < 0){
+						this.play("jump_left");
+					}
+				}
+				
+				if(this.p.vy > 0){
+					if(this.p.vx > 0){
+						this.play("fall_right");
+					} else if(this.p.vx < 0){
+						this.play("fall_left");
+					}
+				}
 			}
+
 		});
 
 
@@ -32,19 +61,89 @@ var Q = window.Q = Quintus()
 			init: function(p) {
 				this._super(p, {
 					 asset: "1up.png",
-					 scale: 0.3,
+					 scale: 1,
 					 x: 20, //respecto a mario
-					 y: -10
+					 y: -10,
+					 sensor: true, //para que los objetos dejen pasar a mario aunque colisionen
+				 	 taken: false
 				 });
+				this.on("sensor", this, "hit");
+				this.add("tween"); //para animar y rotarla
+			},
+			hit: function(collision){
+				if(this.taken) return;
+				if(!collision.isA("Mario")) return;
+				
+				this.taken = true; //para coger las setas y que desaparezcan
+				Q.state.inc("lives", 1);
+				console.log(Q.state.get("lives"));
+				collision.p.vy = -400;
+				
+				this.animate({y: this.p.y-100, angle:360}, //-200 para que la seta gire mas alto
+							  2, //que tarde mas
+							  Q.Easing.Quadratic.Out, //podemos probar animaciones diferntes In, InOut
+							  {callback: function(){this.destroy()}});
+				//this.destroy();
 			}
 		});
 
 
+	// Sprite del enemigo
+	// Create the Enemy class to add in some baddies
+	Q.Sprite.extend("Goomba",{
+		init: function(p) {
+			this._super(p, {
+				 sheet: 'goomba',
+				 x: 400+(Math.random()*200),
+				 y: 250,
+				 frame: 0,
+				 vx: 100
+			 });
 
-	Q.load(["mario_small.png", "mario_small.json", "1up.png", "bg.png", "mapa2021.tmx", "tiles.png" ],  function() {
+			// Enemies use the Bounce AI to change direction
+			// whenver they run into something.
+			this.add('2d, aiBounce, animation');
+			this.on("bump.top", this, "onTop");
+			this.on("bump.left,bump.right,bump.bottom",this, "kill");
+		},
+		onTop: function(collision){
+			if(!collision.obj.isA("Mario")) return;
+			collision.obj.p.vy = -200;
+			console.log("Goomba dies");
+			this.destroy();
+		},
+		kill: function(collision){
+			if(!collision.obj.isA("Mario")) return;
+			collision.obj.p.vy = -200;
+			collision.obj.p.vx = collision.normalX *-500;
+			collision.obj.p.x += collision.normalX *-5;
+			Q.state.dec("lives", 1);
+			console.log(Q.state.get("lives"));
+			if(Q.state.get("lives")<0)
+					collision.obj.destroy();
+		}
+		
+	});
+
+
+
+	Q.load(["mario_small.png", "mario_small.json", "1up.png", "bg.png", "mapa2021.tmx", "tiles.png", "goomba.png", "goomba.json" ],  function() {
 		
 		// Or from a .json asset that defines sprite locations
 		Q.compileSheets("mario_small.png","mario_small.json");
+		Q.compileSheets("goomba.png","goomba.json");
+
+		Q.animations("mario_anim", {
+		walk_right: { frames: [1,2,3], rate: 1/6, next: "parado_r"},
+		walk_left: { frames: [15,16,17], rate: 1/6, next: "parado_l"},
+		jump_right: { frames: [4], rate: 1/6, next: "parado_r" },
+		jump_left: { frames: [18], rate: 1/6, next: "parado_l"},
+		fall_right: { frames:[6,7], rate: 1/6, next: "parado_r" },
+		fall_left: { frames: [21,21], rate: 1/6, next: "parado_l"},
+		parado_r: { frames: [0]},
+		parado_l: { frames: [14]},
+		morir: { frames: [12], loop: false, rate: 1}
+		});
 
 		Q.scene("level1", function (stage){
 			/*
@@ -56,7 +155,7 @@ var Q = window.Q = Quintus()
 	
 			mario = new Q.Mario();
 			stage.insert(mario);
-			stage.insert(new Q.OneUp(), mario); //para que la seta se mueva con mario
+			//stage.insert(new Q.OneUp(), mario); //para que la seta se mueva con mario
 
 			stage.add("viewport").follow(mario,{x: true, y: false}); //la camara sigue a mario centrado en el eje horizontal, en el vertical esta a false
 			stage.viewport.scale = .75; //para acercar mas o menos la camara
@@ -65,6 +164,9 @@ var Q = window.Q = Quintus()
 				mario.destroy(); //para cuando salimos de la escena ya no reciba mas eventos de teclado
 			});
 
+			stage.insert(new Q.Goomba());
+
+			Q.state.reset({lives:2});
 		});
 
 		Q.stageScene("level1");
